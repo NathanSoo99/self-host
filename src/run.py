b47565d9c9c09e740ea0109cef91cb6331af7a09
Nathan <nathansoo99@gmail.com>
2026-07-10 22:24:25 +1000

    moved app to new folder and added basic database connection

--- src/run.py
+++ src/run.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '../data/directory.db')
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World</p>"
+import sqlite3
 
 if __name__ == "__main__":
+    try:
+        conn = sqlite3.connect(filename)
+        print("Hello Database")
+    except sqlite3.Error:
+        print("An Error Occurred")
+    finally:
+        conn.close()
     app.run()
