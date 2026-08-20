b0267e9b0ea6625e40677c578ea5fdeff93b07a1
Nathan <nathansoo99@gmail.com>
2026-08-20 23:13:51 +1000

    added to readme, added file delete database code and started on docstrings

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# self-host
+# Self-Host
+Self hosted cloud storage solution using a virtual file system.
+
+Currently only consists of a very basic virtual command line interface to create and look at virtual directories.
+
+## Running the Program
+1. Clone the repo
+2. Set up and activate virtual environment
+```bash
+cd self-host
+python3 -m venv self-host
+source self-host/bin/activate
+```
+3. Install dependencies
+```bash
+pip install -r requirement.txt
+```
+4. Run program
+```bash
+cd src
+python3 run.py
+```
+
+## Objective
+The idea is for a personal cloud storage solution for hosting locally on a PC or using cloud computing. Originally I planned on making this a mobile project, however due to my lack of familiarity, I have decided on getting started with a desktop version at least for the time being.
+
+This is a personal project which I have wanted to start for a while, however I am quite the procrastinator and I have not been able to do so for a while. As a result I am doing this development formost with maximising movitation and minimising mental load in mind. This means that I am working on whatever seems doable at the time and not doing detailed planning.
+
+## Development Log
+### 20/8/26
+Current progress until start of logs include:
+- Database design to support virtual file system.
+- Functions which inspect the contents of, create, modify and delete directories.
+- Functions which currently read, create, modify and delete file metadata.
--- src/app/virtual_file_system.py
+++ src/app/virtual_file_system.py
@@ -5,10 +5,18 @@ from .database_access import db_connect
 from collections import deque
 
 def traverse_directories(directory_chain, cursor):
+    """
+    Take in a list of directories corresponding to a virtual directory's
+    path and find the database id of that directory if the path is valid.
+    Arguments:
+        directory_chain (list): an ordered list of strings corresponding to a virtual directory's path
+        cursor (Cursor): cursor to the virtual file system database
+    Returns:
+        integer: The database id of the directory if the path is valid and None otherwise
+    """
     current_directory = None
-
-    # traverse path to find directory id
     for directory in directory_chain:
+        # Check current directory for contained directories with next name in the path
         query = """
             SELECT id, parent_id, name
             FROM Directories
@@ -21,6 +29,8 @@ def traverse_directories(directory_chain, cursor):
         }
         cursor.execute(query, query_parameters)
         query_result = cursor.fetchone()
+        # Database constraint on parent_id and name mean that there is 0 or 1 results
+        # Consequently query_result is either the only result or None (invalid path)
         if query_result is None:
             current_directory = None
             break
@@ -30,9 +40,26 @@ def traverse_directories(directory_chain, cursor):
     return current_directory
 
 def get_directory_id(virtual_path, cursor):
+    """
+    Wrapper for traverse_directories() which converts splits the virtual directory's path into
+    directories by splitting it by the "/" character.
+    Arguments:
+        virtual_path (string): Virtual path to a directory of the form ~/d1/d2/...
+        cursor (Cursor): cursor to the virtual file system database
+    Returns:
+        integer: The database id of the directory if the path is valid and None otherwise
+    """
     return traverse_directories(virtual_path.split("/"), cursor)
 
 def get_file_metadata(virtual_path, cursor):
+    """
+    Take in a file's virtual path get its metadata if it exists
+    Arguments:
+        virtual_path (string): Virtual path to a file of the form ~/d1/d2/.../filename
+        cursor (Cursor): cursor to the virtual file system database
+    Returns:
+        dictionary: Python dictionary containing file's metadata
+    """
     path_parts = virtual_path.split("/")
     directory_chain = path_parts[0:-1]
     filename = path_parts[-1]
@@ -40,15 +67,16 @@ def get_file_metadata(virtual_path, cursor):
 
     directory_id = traverse_directories(directory_chain, cursor)
     if directory_id is not None:
-        query = "SELECT id, name, uuid FROM Files WHERE name = :filename AND directory_id = :directory_id"
+        query = "SELECT id, directory_id, name, uuid FROM Files WHERE name = :filename AND directory_id = :directory_id"
         query_parameters = {"filename": filename, "directory_id": directory_id}
         cursor.execute(query, query_parameters)
         query_result = cursor.fetchone()
         if query_result is not None:
             result = {
                 "id": query_result[0],
-                "name": query_result[1],
-                "uuid": query_result[2]
+                "directory_id": query_result[1],
+                "name": query_result[2],
+                "uuid": query_result[3]
             }
 
     return result
@@ -240,7 +268,7 @@ def add_file(directory_path, filename, content):
     directory_id = get_directory_id(directory_path, cursor)
     if directory_id is not None:
         file_identifier = uuid.uuid4().bytes
-        # TODO write content to physical disk with file identifier
+        # TODO write content to os file system with file identifier
         print(content)
         query = "INSERT INTO Files (directory_id, name, uuid) VALUES (:directory_id, :name, :uuid) RETURNING *"
         query_data = {
@@ -298,8 +326,23 @@ def modify_file_name(path, new_name):
     conn.close()
     return result
 
-def delete_file():
-    pass
+def delete_file(path):
+    conn = db_connect(db_filename)
+    if conn is None:
+        return None
+    result = False
+    cursor = conn.cursor()
+    file_metadata = get_file_metadata(path, cursor)
+    if file_metadata is not None:
+        # TODO delete from os filesystem
+        query = "DELETE FROM Files WHERE id = :file_id"
+        query_parameters = {"file_id": file_metadata["id"]}
+        cursor.execute(query, query_parameters)
+        result = True
+    cursor.close()
+    conn.commit()
+    conn.close()
+    return result
 
 """
     row = cursor.fetchone()
